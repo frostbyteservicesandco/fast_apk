ea5196920c5423fa9f64f67ad73d774658bbc95c
frostbyteservicesandco <frostbyteservicesandco@gmail.com>
2026-01-16 01:21:11 +0530

    Update supabase_config.dart

--- lib/config/supabase_config.dart
+++ lib/config/supabase_config.dart
@@ -3,3 +3,4 @@ class SupabaseConfig {
   static const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrbGpmcGZpZnJtZmR5a295cGV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0MjUxNjksImV4cCI6MjA4NDAwMTE2OX0.eEoBo2MEkgadPmR3kkeg6dNHAAuVuz7xc4DfxPGlsZA';
 }
 
+
